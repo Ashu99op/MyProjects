5086045210ffbb8e66f0dbd4d215ac873721bba2
Ashu99op <ashucode0@gmail.com>
2023-03-27 22:37:53 +0530

    Start netflix clopne

---
+++ netflix/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBxvAxJ-bnA8UV6THj4LBEdZqtUdunQtH8",
+    authDomain: "netflix-clone-82f0d.firebaseapp.com",
+    projectId: "netflix-clone-82f0d",
+    storageBucket: "netflix-clone-82f0d.appspot.com",
+    messagingSenderId: "1017624280285",
+    appId: "1:1017624280285:web:3da58f19ed9f701e5fef34",
+    measurementId: "G-79ZJXK9698"
+  };
